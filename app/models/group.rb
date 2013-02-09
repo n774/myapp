5e75c91a907747ea40a041c3176920aef62f0833
a1125ts <a1125ts.aiit.ac.jp>
2013-02-09 15:17:47 +0900

    グループ管理画面のviewを修正しました

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,8 @@
 class Group < ActiveRecord::Base
   attr_accessible :g_id, :g_name
+
+  validates :g_id, :presence => true
+  validates :g_name, :presence => true
+  validates_uniqueness_of :g_id
+  validates_uniqueness_of :g_name
 end
