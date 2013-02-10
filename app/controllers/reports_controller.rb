f21377af25a47dbb225e93b8a75dc5f0adf76f3c
Ikuhide Nara <nara@nara-VirtualBox.(none)>
2013-02-10 21:50:55 +0900

    add graph

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,7 +12,7 @@ class ReportsController < ApplicationController
   def show
     @reports = Rate.find_by_sql(['select r.g_id, g.g_name,  r.rate_id, u1.u_name as rate_name,  r.brate_id, u2.u_name as brate_name, r.point, r.comment  from rates r inner join groups g on r.g_id = g.g_id inner join users u1 on r.rate_id = u1.u_id inner join users u2 on r.brate_id = u2.u_id  where r.g_id = ? order by brate_id', params[:id]])
 
-##    @lists = Rate.find_by_sql(['SELECT r.user_id, u.username FROM RATES r INNER JOIN USERS u ON u.id = r.user_id where group_id = ? group by user_id', params[:id]])
+    @lists = Rate.find_by_sql(['SELECT r.brate_id, u.u_name as brate_name FROM RATES r INNER JOIN USERS u ON u.u_id = r.brate_id where r.g_id = ? group by brate_id order by brate_id', params[:id]])
 
     respond_to do |format|
       format.html # show.html.erb
